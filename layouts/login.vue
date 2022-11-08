3cc60dc467f524d3d03c241fc8e1d5025756386b
Firbert Oktariko <firbert96@gmail.com>
2022-11-09 01:39:38 +0700

    final

---
+++ layouts/login.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="bg-white">
+      <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <section
+          class="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6"
+        >
+          <img
+            alt="Night"
+            src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            class="absolute inset-0 h-full w-full object-cover opacity-80"
+          />
+  
+          <div class="hidden lg:relative lg:block lg:p-12">
+            <h2
+              class="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl"
+            >
+              Login
+            </h2>
+          </div>
+        </section>
+  
+            <main
+            aria-label="Main"
+            class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6"
+            >
+                <Nuxt/>
+            </main>
+        </div>
+    </section>
+</template>
---
+++ layouts/register.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bg-white">
+      <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <aside
+          class="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6"
+        >
+          <img
+            alt="Pattern"
+            src="https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            class="absolute inset-0 h-full w-full object-cover"
+          />
+        </aside>
+        <main
+          aria-label="Main"
+          class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6"
+        >
+            <Nuxt/>
+        </main>
+      </div>
+    </div>
+</template>
